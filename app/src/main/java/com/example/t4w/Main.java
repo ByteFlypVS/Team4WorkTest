9ab2c6f49889024326e56a06b3245e5305d1c1b5
byteflypvs <c21344786@mytudublin.ie>
2024-04-18 10:35:25 +0100

    Checkpoint: Base Files

---
+++ app/src/main/java/com/example/t4w/Main.java
@@ -0,0 +1,12 @@
+package com.example.t4w;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
