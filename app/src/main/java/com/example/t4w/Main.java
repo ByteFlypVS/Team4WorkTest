cc25734f8cdf62c1a2037717f8adc2ea9bb1a815
byteflypvs <c21344786@mytudublin.ie>
2024-04-19 01:45:29 +0100

    Checkpoint: Homepage Implementation

--- app/src/main/java/com/example/t4w/Main.java
+++ app/src/main/java/com/example/t4w/Main.java
@@ -4,11 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class Main extends AppCompatActivity{
+    LinearLayout skills, interests, experience, about;
+    Button search;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        skills = findViewById(R.id.hSkill);
+        interests = findViewById(R.id.hInterest);
+        experience = findViewById(R.id.hExp);
+        about = findViewById(R.id.hAbout);
+        search = findViewById(R.id.hSearch);
+
+        skills.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intSkill = new Intent(Main.this, SkillPage.class);
+                startActivity(intSkill);
+            }
+        });
+
+        interests.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intInterest = new Intent(Main.this, InterestPage.class);
+                startActivity(intInterest);
+            }
+        });
+
+        search.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intSearch = new Intent(Main.this, JobPage.class);
+                startActivity(intSearch);
+            }
+        });
     }
 }
