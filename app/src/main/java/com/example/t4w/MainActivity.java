77febf17236f095600cbaceb2211bb90669ba052
Kvsaii <jachont1@yahoo.com>
2024-04-25 16:26:24 +0100

    Comment testing and testing Github.

--- app/src/main/java/com/example/t4w/MainActivity.java
+++ app/src/main/java/com/example/t4w/MainActivity.java
@@ -6,8 +6,9 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+//Testing Comment
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
